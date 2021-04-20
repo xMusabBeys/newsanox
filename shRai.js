5dcbb21997fa275a3760379f2bd7737a399f10d5
xMusabBeys <79603142+xMusabBeys@users.noreply.github.com>
2021-04-20 13:09:05 +0300

    Add files via upload

---
+++ shRai.js
@@ -0,0 +1,13 @@
+const { ShardingManager } = require('discord.js');
+
+const bumbe = new ShardingManager('./raid.js', { //main dosyanızı yazın!
+	totalShards: 2, //Auto yazılabilir veya farklı bir sayı yazabilirsiniz.
+    token: "ODAwMzA2ODIxMzYxMTcyNDkx.YAQNzw.oeQ6WfJWAHxlFnTX4rJG5AgFg2c" //Tokeninizi giriniz
+});
+bumbe.spawn();
+
+bumbe.on('shardCreate', shard => {
+    console.log(`${shard.id} İDli shard başlatıldı!`);
+});
+
+//Discord.js v12 İçindir. 
